87346b5111af3b62d18598c37b984a1e8d0c947e
meghajindal06 <megha.6sagi@gmail.com>
2015-09-24 21:59:04 +0200

    added materialize

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform , $rootScope, $window, sAuth) {
+.run(function($ionicPlatform , $rootScope, $window) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -22,73 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
   
-  $rootScope.user = {};
-
-  $window.fbAsyncInit = function() {
-    // Executed when the SDK is loaded
-
-    FB.init({ 
-
-      /* 
-       The app id of the web app;
-       To register a new app visit Facebook App Dashboard
-       ( https://developers.facebook.com/apps/ ) 
-      */
-
-      appId: '***************', 
-
-      /* 
-       Adding a Channel File improves the performance 
-       of the javascript SDK, by addressing issues 
-       with cross-domain communication in certain browsers. 
-      */
-
-      channelUrl: 'app/channel.html', 
-
-      /* 
-       Set if you want to check the authentication status
-       at the start up of the app 
-      */
-
-      status: true, 
-
-      /* 
-       Enable cookies to allow the server to access 
-       the session 
-      */
-
-      cookie: true, 
-
-      /* Parse XFBML */
-
-      xfbml: true 
-    });
-
-    sAuth.watchAuthenticationStatusChange();
-
-  };
-
-  // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
-
-  (function(d){
-    // load the Facebook javascript SDK
-
-    var js, 
-    id = 'facebook-jssdk', 
-    ref = d.getElementsByTagName('script')[0];
-
-    if (d.getElementById(id)) {
-      return;
-    }
-
-    js = d.createElement('script'); 
-    js.id = id; 
-    js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
-
-    ref.parentNode.insertBefore(js, ref);
-
-  }(document));
+ 
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
